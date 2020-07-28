137a85005acf75c146440d3644024fd69a189403
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-07-29 00:16:09 +0900

    Add note to freq function

--- tune.py
+++ tune.py
@@ -4,19 +4,44 @@ import enum
 import os
 import time
 
+class NoteLabel(enum.Enum):
+    DO = "DO",
+    RE = "RE",
+    MI = "MI",
+    FA = "FA",
+    SOL = "SOL",
+    LA = "LA",
+    SI = "SI"
+
+class NoteShift(enum.Enum):
+    SHARP = "#"
+    BEMOL = "b"
+
 #Intervals to LA3
 interval_dict = {
-    "DO" : -9,
-    "RE" : -7,
-    "MI" : -5,
-    "FA" : -4,
-    "SOL" : -2,
-    "LA" : 0,
-    "SI" : 2
+    NoteLabel.DO : -9,
+    NoteLabel.RE : -7,
+    NoteLabel.MI : -5,
+    NoteLabel.FA : -4,
+    NoteLabel.SOL : -2,
+    NoteLabel.LA : 0,
+    NoteLabel.SI : 2
 }
 
-def note_label_to_freq(note_name, note_shift, octave):
-    pass
+def note_label_to_freq(note_name, octave, note_shift=None):
+    shifter = 0
+    if note_shift == NoteShift.SHARP:
+        shifter = 1
+    if note_shift == NoteShift.BEMOL:
+        shifter = -1
+
+    semitone_freq = 1.05946
+    la3_freq = 440
+
+    shifted_octave = octave-3
+    la3_interval = shifted_octave*12 + interval_dict[note_name] + shifter
+
+    return la3_freq * (semitone_freq**la3_interval)
 
 class Pitch(enum.Enum):
     MI2 = 164.8
