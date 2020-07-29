66b2773f841e539fe481e801ecb0e84110bd9274
Nicolas Busser <nicolas.busser67@gmail.com>
2020-07-29 02:35:09 -0700

    Add secondary names to arguments

--- music_reader.py
+++ music_reader.py
@@ -7,13 +7,13 @@ import sheet_parser as sp
 def main():
     parser = argparse.ArgumentParser(description="Plays a music sheet")
     parser.add_argument("filename", help="source sheet file")
-    parser.add_argument("--string", action="store_true", help="read the music from a string")
-    parser.add_argument("-t", help="tempo multiplier")
+    parser.add_argument("-s", "--string", action="store_true", help="read the music from a string")
+    parser.add_argument("-t", "--tempo", help="tempo multiplier")
     args = parser.parse_args()
 
     tempo = 1
-    if args.t:
-        tempo = 1/float(args.t)
+    if args.tempo:
+        tempo = 1/float(args.tempo)
 
     if args.string:
         sheet = args.filename
