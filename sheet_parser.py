c85a4c440d8591713ad5a48685427f968c9b0196
Nicolas Busser <nicolas.busser67@gmail.com>
2020-07-30 22:56:27 -0700

    Embed silence to pattern

--- sheet_parser.py
+++ sheet_parser.py
@@ -143,7 +143,10 @@ class SheetVisitor(parsimonious.nodes.NodeVisitor):
         self.current_pitch_sleep_time = None
 
     def visit_silence(self, node, children):
-        self.current_tune = tune.Silence(self.current_pitch_sleep_time)
+        local_pattern = tune.Pattern([])
+        local_pattern.add_tune(tune.Silence(self.current_pitch_sleep_time))
+
+        self.current_tune = local_pattern
 
 def parse_sheet(sheet):
     tree = grammar.parse(sheet)
